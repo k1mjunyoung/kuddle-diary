5dc85822b9cf8fa3f446c65279fd5f2257efc682
k1mjunyoung <me@kimjunyoung.com>
2024-01-23 11:42:54 +0900

    Initial commit

---
+++ src/main/java/KuddleDiary/KuddleDiaryApplication.java
@@ -0,0 +1,13 @@
+package KuddleDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuddleDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuddleDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KuddleDiary/KuddleDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package KuddleDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuddleDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
